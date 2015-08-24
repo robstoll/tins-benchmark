9f7ee49fd23d011876e92a7a9c46641b15fef349
Robert Stoll <rstoll@tutteli.ch>
2015-08-24 13:28:32 +0200

    TINS-637 benchmark program
    Rewrote the test in order that I can run both tinsphp and java at once. Furthermore, it makes more sense if they are executed alternately because this way they are  more equally influenced by other processes on the computer.

--- src/ch/tsphp/tinsphp/benchmark/Main.java
+++ src/ch/tsphp/tinsphp/benchmark/Main.java
@@ -10,39 +10,66 @@ import java.io.PrintWriter;
 public class Main
 {
     public static void main(String[] args) throws IOException, InterruptedException {
-        PrintWriter writer = new PrintWriter("tmp/times.txt", "UTF-8");
-        ProcessBuilder builder = new ProcessBuilder("java",
+        PrintWriter writer1 = new PrintWriter("tmp/times-tinsphp.txt", "UTF-8");
+        PrintWriter writer2 = new PrintWriter("tmp/times-java.txt", "UTF-8");
+        ProcessBuilder builder1 = new ProcessBuilder("java",
                 "-cp", "\"D:\\tins-benchmark\\lib\\*\"",
                 "ch.tsphp.tinsphp.Main",
                 "\"D:\\tins-benchmark\\src\\test.php\"",
                 "\"D:\\tins-benchmark\\tmp\\test.tsphp\"");
-//        ProcessBuilder builder = new ProcessBuilder("javac",
-//                "-d", "\"D:\\tins-benchmark\\tmp\"",
-//                "\"D:\\tins-benchmark\\src\\Test.java\"");
-        builder.redirectErrorStream(true);
-        for (int i = 0; i < 1010; ++i) {
+        builder1.redirectErrorStream(true);
+        ProcessBuilder builder2 = new ProcessBuilder("javac",
+                "-d", "\"D:\\tins-benchmark\\tmp\"",
+                "\"D:\\tins-benchmark\\src\\Test.java\"");
+        builder2.redirectErrorStream(true);
+        for (int i = 0; i < 1; ++i) {
             long start = System.nanoTime();
-            Process process = builder.start();
+            Process process;
+            if (i % 2 == 0) {
+                process = builder1.start();
+            } else {
+                process = builder2.start();
+            }
             int errCode = process.waitFor();
             long stop = System.nanoTime();
-            if (errCode == 0) {
-                if (i >= 10) {
-                    writer.println(stop - start);
-                    System.out.println(i + ": " + (stop - start));
-                }
-            } else {
-                System.err.println("Error occurred");
-                output(process.getInputStream());
+            if (writeOutput(writer1, writer2, i, start, stop, process, errCode)) {
                 break;
             }
-            File file = new File("D:\\tins-benchmark\\tmp\\test.tsphp");
+            File file;
+            if (i % 2 == 0) {
+                file = new File("D:\\tins-benchmark\\tmp\\test.tsphp");
+            } else {
+                file = new File("D:\\tins-benchmark\\tmp\\Test.class");
+            }
             file.delete();
         }
-        writer.close();
+        writer1.close();
         System.out.println("tests done");
     }
 
-    private static void output(InputStream inputStream) throws IOException {
+    private static boolean writeOutput(PrintWriter writer1, PrintWriter writer2, int i, long start, long stop, Process process,
+            int errCode) throws IOException {
+        if (errCode == 0) {
+            if (i % 2 == 0) {
+                if (i >= 20) {
+                    writer1.println(stop - start);
+                }
+                System.out.println("tinsphp " + i + ": " + (stop - start));
+            } else {
+                if (i >= 20) {
+                    writer2.println(stop - start);
+                }
+                System.out.println("java " + i + ": " + (stop - start));
+            }
+        } else {
+            System.err.println("Error occurred");
+            writeToErr(process.getInputStream());
+            return true;
+        }
+        return false;
+    }
+
+    private static void writeToErr(InputStream inputStream) throws IOException {
         BufferedReader br = null;
         try {
             String lineSeparator = System.getProperty("line.separator");
